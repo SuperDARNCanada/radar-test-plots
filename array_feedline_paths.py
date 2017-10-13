9015f685a12dd2d729a13f31f0236c087d497d23
Keith Kotyk <keithkotyk@gmail.com>
2017-10-13 10:20:42 -0600

    Fixed some issues with csv refactor
    csv.DictReader was not working with our non standard CSV files. I changed it to use the default reader. I then needed an error check to make sure the reader never failed to correctly parse a line which happened with one file in one line.

--- array_feedline_paths.py
+++ array_feedline_paths.py
@@ -25,8 +25,6 @@ vswr_intf_files_str = sys.argv[5]
 S12_arrays_plot_title = radar_name + ' Combined Main and IF Arrays \nAntenna/Feedlines'
 data_description = 'This data was taken on site visits in August 2017.'
 
-number_of_data_points = 801
-
 sys.path.append(data_location)
 
 with open(vswr_main_files_str) as f:
@@ -76,16 +74,19 @@ def main():
     intf_data = {}
     for k,v in vswr_main_files.iteritems():
         with open(data_location + v, 'r') as csvfile:
-            while not 'Freq. [Hz]' in csvfile.readline():
+            while not 'Freq. [Hz]' in csvfile.readline(): #skip to header
                 pass
-            next(csvfile)
-            csv_reader = csv.DictReader(csvfile)
+            next(csvfile) #skip over header
+            csv_reader = csv.reader(csvfile)
 
             data = []
             for row in csv_reader:
-                freq = row[0]
-                VSWR = row[1]
-                phase = row[2]
+                try:
+                    freq = float(row[0])
+                except:
+                    continue
+                VSWR = float(row[1])
+                phase = float(row[2])
 
                 return_loss = 20 * math.log(((VSWR + 1) / (VSWR - 1)),
                                             10)  # this is dB measured at instrument.
@@ -116,9 +117,26 @@ def main():
                 receive_power = transmission - cable_loss  # power incoming from antenna will have mismatch point and then cable losses.
                 # print "Received Power from Incident at Antenna: {}".format(receive_power)
                 receive_power = round(receive_power, 5)
-                data.append((freq, receive_power, float(phase) / 2))
+                data.append({'freq':freq, "receive_power": receive_power, "phase": float(phase) / 2})
             main_data[k] = data
 
+
+
+    for k,v in vswr_intf_files.iteritems():
+        with open(data_location + v, 'r') as csvfile:
+            while not 'Freq. [Hz]' in csvfile.readline(): #skip to header
+                pass
+            next(csvfile) #skip over header
+            csv_reader = csv.reader(csvfile)
+
+            data = []
+            for row in csv_reader:
+                try:
+                    freq = float(row[0])
+                except:
+                    continue
+                VSWR = float(row[1])
+                phase = float(row[2])
 
             # lines = csvfile.readlines()
             # for ln in lines:
@@ -126,6 +144,7 @@ def main():
             #         header_line = ln
             #         break
             # find_phase = header_line.split(",", 16)
+            # print i
             # # print find_phase
             # freq_column = find_phase.index('Freq. [Hz]')
             # vswr_column = find_phase.index('VSWR [(VSWR)]')
@@ -148,91 +167,16 @@ def main():
             #     vswr_column = find_phase.index('VSWR [(VSWR)]') + 4 * it
             #     phase_column = find_phase.index(phase_value) + 4 * it
             # # print freq_column, vswr_column, phase_column
-            # main_data[i] = np.zeros((len(lines) - 46,),
+            # intf_data[i] = np.zeros((len(lines) - 46,),
             #                         dtype=[('freq', 'i4'), ('receive_power', 'f4'),
             #                                ('phase', 'f4')])
-            # for ln in range(lines.index(header_line) + 1, lines.index(header_line) +
-            #         number_of_data_points + 1):
+            # for ln in range(lines.index(header_line) + 1, lines.index(header_line) + number_of_data_points + 1):
             #     data = lines[ln].split(",", 16)
-            #     # only taking the same sweep data (3 columns in a row)
+            #     # if ln==lines.index(header_line)+1:
+            #     #    print data
+            #     # only taking first sweep data (columns 1-3)
+            #     # print data[vswr_column]
             #     VSWR = float(data[vswr_column])
-            #     # reflection_coeff=abs((VSWR-1)/(VSWR+1))
-            #     # z_balun_junction=50*abs((1+reflection_coeff)/(1-reflection_coeff)) # 50 ohm cable is Z-naught.
-            #     # print VSWR
-
-            #     return_loss = 20 * math.log(((VSWR + 1) / (VSWR - 1)),
-            #                                 10)  # this is dB measured at instrument.
-            #     # calculate mismatch error. need to adjust power base to power at antenna mismatch.
-            #     power_outgoing = 10 ** (-cable_loss / 10)  # ratio to 1, approaching the balun point.
-            #     # taking into account reflections for mismatch at antenna = S11
-            #     # get single-direction data by making the power base = power_outgoing (incident at mismatch point at balun).
-
-            #     reflected_loss = -return_loss + cable_loss  # dB, at mismatch point that is reflected.
-
-            #     returned_power = 10 ** (reflected_loss / 10)
-            #     if returned_power > power_outgoing:
-            #         print "Antenna: {}".format(i)
-            #         print data[freq_column]
-            #         print "wRONG"
-            #         print returned_power, reflected_loss
-            #         print power_outgoing, cable_loss
-            #     try:
-            #         reflection = 10 * math.log((returned_power / power_outgoing), 10)
-            #         transmission = 10 * math.log((1 - (returned_power / power_outgoing)), 10)
-            #     except ValueError:
-            #         reflection = 0
-            #         transmission = -10000
-            #     # this is single direction reflection at mismatch point, assume this happens on
-            #     # incoming receives as well at this point (reflection coefficient is same in both directions)
-            #     # what is the transmitted power through that point then (which is relative the signal incident upon it)?
-
-            #     receive_power = transmission - cable_loss  # power incoming from antenna will have mismatch point and then cable losses.
-            #     # print "Received Power from Incident at Antenna: {}".format(receive_power)
-            #     receive_power = round(receive_power, 5)
-            #     main_data[i][ln - 46] = (
-            #     data[freq_column], receive_power, float(data[phase_column]) / 2)
-
-    for i in vswr_intf_files.keys():
-        with open(data_location + vswr_intf_files[i], 'r') as csvfile:
-            lines = csvfile.readlines()
-            for ln in lines:
-                if ln.find('Freq. [Hz]') != -1:  # found the right line
-                    header_line = ln
-                    break
-            find_phase = header_line.split(",", 16)
-            print i
-            # print find_phase
-            freq_column = find_phase.index('Freq. [Hz]')
-            vswr_column = find_phase.index('VSWR [(VSWR)]')
-            phase_value = 'Phase [\xb0]'
-            try:
-                phase_column = find_phase.index(phase_value)
-            except ValueError:
-                phase_value = 'Phase [\xef\xbf\xbd]'
-                phase_column = find_phase.index(phase_value)
-            it = 0
-            while (abs(vswr_column - freq_column) > 2) or (abs(phase_column - freq_column) > 2):
-                # data is from different sweeps (not the first sweep)
-                find_phase.remove('')
-                find_phase.remove(find_phase[2])
-                find_phase.remove(find_phase[1])
-                find_phase.remove(find_phase[0])
-                # print find_phase
-                it = it + 1
-                freq_column = find_phase.index('Freq. [Hz]') + 4 * it
-                vswr_column = find_phase.index('VSWR [(VSWR)]') + 4 * it
-                phase_column = find_phase.index(phase_value) + 4 * it
-            # print freq_column, vswr_column, phase_column
-            intf_data[i] = np.zeros((len(lines) - 46,),
-                                    dtype=[('freq', 'i4'), ('receive_power', 'f4'),
-                                           ('phase', 'f4')])
-            for ln in range(lines.index(header_line) + 1, lines.index(header_line) + number_of_data_points + 1):
-                data = lines[ln].split(",", 16)
-                # if ln==lines.index(header_line)+1:
-                #    print data
-                # only taking first sweep data (columns 1-3)
-                # print data[vswr_column]
-                VSWR = float(data[vswr_column])
                 return_loss = 20 * math.log(((VSWR + 1) / (VSWR - 1)), 10)
                 # calculate mismatch error. need to adjust power base to power at antenna mismatch.
                 power_outgoing = 10 ** (-cable_loss / 10)  # ratio to 1.
@@ -250,14 +194,15 @@ def main():
                 receive_power = round(receive_power, 5)
                 # receive_power=-return_loss
                 # convert S11 phase offset into a single direction (/2)
-                intf_data[i][ln - 46] = (
-                data[freq_column], receive_power, float(data[phase_column]) / 2)
+                data.append({'freq':freq, "receive_power": receive_power, "phase": float(phase) / 2})
                 # time.sleep(1)
                 # print main_data[i][ln-46]['freq']
+            intf_data[k] = data
 
     # now we have data points at same frequencies.
     # next - sum signals.
-    combined_main_array = np.zeros((number_of_data_points,), dtype=[('freq', 'i4'), ('receive_power', 'f4'), ('phase', 'f4')])
+    number_of_data_points = len(main_data[1])
+    combined_main_array = np.zeros(number_of_data_points, dtype=[('freq', 'i4'), ('receive_power', 'f4'), ('phase', 'f4')])
     for i in range(0, number_of_data_points):
         combined_main_array[i]['phase'] = main_data[1][i]['phase']
         combined_main_array[i]['freq'] = main_data[1][i]['freq']
@@ -294,6 +239,7 @@ def main():
 
     # do the same for the interferometer array.
     combined_intf_array = np.zeros((number_of_data_points,), dtype=[('freq', 'i4'), ('receive_power', 'f4'), ('phase', 'f4')])
+    print len(intf_data[1])
     for i in range(0, number_of_data_points):
         combined_intf_array[i]['phase'] = intf_data[1][i]['phase']
         combined_intf_array[i]['freq'] = intf_data[1][i]['freq']
